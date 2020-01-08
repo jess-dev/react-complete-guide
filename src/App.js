19d33310519254148756810c26fb517e6ff5cc2f
Jessica Taylor <jess@192.168.1.5>
2020-01-08 00:05:37 +0000

    Props stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Person from './Person/Person';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Does this work??</h1>
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <Person name="Jess" age="35" />
+      <Person name="Jamie" age="15">My Hobbies: Gaming</Person>
+      <Person name="MJ" age="10" />
     </div>
   );
 }
